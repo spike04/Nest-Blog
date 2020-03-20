afab32a730af6480128ef347b4237c11586f3e2e
Rubin Bajracharya <fm-pc-lt-132@fm-pc-lt-132s-MacBook-Pro.local>
2020-03-20 10:08:01 +0545

    Added Database Connection Code for PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+import { DatabaseConnectionService } from './database-connection.service'
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConnectionService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
